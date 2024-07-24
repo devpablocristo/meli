4b53cdaac15991f1787158ca835caffcd34c07bf
devpablocristo <devpablocristo@gmail.com>
2024-07-24 18:34:03 -0300

    dns mongo fix

--- api-items/pkg/mongodb/mongo-driver/mongodb-config.go
+++ api-items/pkg/mongodb/mongo-driver/mongodb-config.go
@@ -14,14 +14,6 @@ type MongoDBClientConfig struct {
 // dsn gera o Data Source Name (DSN) a partir da configuração fornecida
 
 func (config MongoDBClientConfig) dsn() string {
-
-	dns := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?authMechanism=SCRAM-SHA-256",
-		config.User, config.Password, config.Host, config.Port, config.Database)
-
-	fmt.Println(dns)
-
 	return fmt.Sprintf("mongodb://%s:%s@%s:%s/%s?authMechanism=SCRAM-SHA-256",
 		config.User, config.Password, config.Host, config.Port, config.Database)
-
-	//return "mongodb://root:root@mongodb:27017"
 }
